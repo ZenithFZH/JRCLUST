415b81e8c7648779a56e3e662187c81b2ae36330
Zhehan Fu <zhehfu@student.ethz.ch>
2026-06-29 17:58:36 +0200

    Prevent stale axes from opening blank figure

--- +jrclust/+views/@Figure/addAxes.m
+++ +jrclust/+views/@Figure/addAxes.m
@@ -1,14 +1,16 @@
 function hAx = addAxes(obj, axKey, varargin)
     %ADDAXES Create and store new axes for this figure
     if ~obj.isReady
-        obj.hFig = figure();
-    end
-
-    if obj.hasAxes(axKey)
+        hAx = [];
         return;
     end
 
-    hAx = axes(obj.hFig, varargin{:});
+    if obj.hasAxes(axKey)
+        hAx = obj.hAxes(axKey);
+        return;
+    end
+
+    hAx = axes('Parent', obj.hFig, varargin{:});
     obj.hAxes(axKey) = hAx;
     obj.axApply(axKey, @hold, 'on');
 end
